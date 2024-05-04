3a00ad52431b3ecdddfe2388131a0e7a8195c89f
pavan6115 <kulkarnipavan6115@gmail.com>
2024-05-04 16:39:32 +0530

    added react element

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'Child11'),
-    React.createElement('h2', {}, 'Child12'),
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, 'Child21'),
-    React.createElement('h2', {}, 'Child22'),
-  ]),
-])
+// React Element --> It is an object
+// JSX -> React Element -> Object -> HTMLRenderer
+const jsxHeading = (
+  <h1 className='heading'>
+    Hello from jsx in App.js 🚀
+    <div>
+      <em>hello from em</em>
+    </div>
+  </h1>
+)
+console.log(jsxHeading)
+
+
+
+
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(parent)
+root.render(jsxHeading)
